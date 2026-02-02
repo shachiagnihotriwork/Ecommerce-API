05de1a32e96700f060abc863970b1090a8204060
Shachiagnihotriwork <Shachiagnihotriwork@gmail.com>
2026-02-02 05:44:28 +0000

    Add database connection

---
+++ database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase , sessionmaker
+
+db_url = "postgresql+psycopg2://shachi:7023690737@localhost:5432/ecommerce"
+engine = create_engine(db_url)
+
+class Base(DeclarativeBase):
+    pass
+
+session = sessionmaker(autocommit = False , autoflush = False, bind = engine)
